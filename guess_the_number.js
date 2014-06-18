d7f90caff6b8d1589d76521c1b7861ea8c89fc38
Jacinda Zhong <jacindaz@gmail.com>
2014-06-18 19:59:18 -0400

    Working version with guess number, and shows number onclick
     - tells user (with the name they entered) if they won or lost
     - hides div, but shows if user wants to know the answer

--- guess_the_number.js
+++ guess_the_number.js
@@ -1,3 +1,34 @@
+function guessNumber(){
+  var name = document.guess_number.user_name.value;
+  var range = document.guess_number.user_range.value;
+  var guess = document.guess_number.user_guess.value;
+  var number = Math.floor(Math.random() * range);
+
+  if (guess === number) {
+    alert("Congratulations " + name + ", you won!");
+  } else {
+    alert("I'm sorry " + name + ", you guessed incorrectly.");
+  }
+
+  // document.getElementById('tell_me').value = number;
+  return number;
+}
+
+function showDiv() {
+  div = document.getElementById('hide-div');
+  div.style.display = "block"
+}
+
+function returnRandomNumber(){
+  window.alert = function() {};
+  random_number = guessNumber();
+  document.getElementById('tell_me').value = random_number;
+}
+
+
+/*********************UNUSED FUNCTIONS*************************/
+
+
 function displayRandomNumber(){
   var range = prompt("What is highest number you would like to generate?");
   var number = Math.floor(Math.random() * range);
