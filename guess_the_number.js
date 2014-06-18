65f2b8cbd645cd4f6f426d1d74077c3b693d396a
Jacinda Zhong <jacindaz@gmail.com>
2014-06-18 18:53:52 -0400

    Generates random number from range (prompt) and alert

--- guess_the_number.js
+++ guess_the_number.js
@@ -1,4 +1,16 @@
 function displayRandomNumber(){
-  var number = Math.floor(Math.random() * 100);
+  range = prompt("What is highest number you would like to generate?");
+  var number = Math.floor(Math.random() * range);
   alert("Your random number is: " + number);
+}
+
+function displayRandomNumForm(){
+  var range = document.generate_num.user_range.value;
+  var random_num = Math.floor(Math.random() * range);
+  document.getElementById('generate').value = random_num;
+}
+
+function getUserName(){
+  name = prompt("What is your name?");
+  alert("Hello " + name + "!");
 }
