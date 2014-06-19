b7820793e207ad4eaa4ee7be5163e1e45e42e8a7
Jacinda Zhong <jacindaz@gmail.com>
2014-06-18 20:00:26 -0400

    Cleans up, removes unused js functions

--- guess_the_number.js
+++ guess_the_number.js
@@ -10,7 +10,6 @@ function guessNumber(){
     alert("I'm sorry " + name + ", you guessed incorrectly.");
   }
 
-  // document.getElementById('tell_me').value = number;
   return number;
 }
 
@@ -23,30 +22,4 @@ function returnRandomNumber(){
   window.alert = function() {};
   random_number = guessNumber();
   document.getElementById('tell_me').value = random_number;
-}
-
-
-/*********************UNUSED FUNCTIONS*************************/
-
-
-function displayRandomNumber(){
-  var range = prompt("What is highest number you would like to generate?");
-  var number = Math.floor(Math.random() * range);
-  alert("Your random number is: " + number);
-}
-
-function displayRandomNumForm(){
-  var range = document.generate_num.user_range.value;
-  var random_num = Math.floor(Math.random() * range);
-  document.getElementById('generate').value = random_num;
-}
-
-function getUserName(){
-  var name = document.say_hi.user_name.value;
-  alert("Hello " + name + ", nice to meet you!");
-}
-
-function reproduceNumber(){
-  var user_number = prompt("What is your phone number?")
-  alert("Your phone number is: " + user_number)
 }
